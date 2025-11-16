b4cbea304df6b1790d64bbe04e2c73a165a90037
Jose A. Genis <jagenis@gmail.com>
2025-11-16 12:37:06 +0100

    añadido l8_boxlayout

--- l8_boxlayout.py
+++ l8_boxlayout.py
@@ -5,9 +5,9 @@ from kivy.uix.button import Button
 class DemoBoxLayout(App):
     def build(self):
         layout = BoxLayout(orientation="horizontal", spacing=10, padding=10)
-        layout.add_widget(Button(text="Botón 1"))
-        layout.add_widget(Button(text="Botón 2"))
-        layout.add_widget(Button(text="Botón 3"))
+        layout.add_widget(Button(text="Botón 1", size_hint=(2, 1)))
+        layout.add_widget(Button(text="Botón 2", size_hint=(0.25, 1)))
+        layout.add_widget(Button(text="Botón 3", size_hint=(1, 1)))
         return layout
 
 DemoBoxLayout().run()
